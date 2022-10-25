01231a6b65fa4e799ab2804d86361d639d725241
samuel-satter <97049361+samuel-satter@users.noreply.github.com>
2022-10-25 14:54:25 +0200

    Added LineModel and CircleModel classes and started building scene in scenebuilder

--- main/src/main/java/com/example/main/models/SquareModel.java
+++ main/src/main/java/com/example/main/models/SquareModel.java
@@ -2,23 +2,13 @@ package com.example.main.models;
 
 public class SquareModel {
     private Position position;
-    private Direction direction;
 
-    public SquareModel(Position position, Direction direction){
+    public SquareModel(Position position){
         this.position = position;
-        this.direction = direction;
     }
 
     public SquareModel(){
         this.position = new Position(19, 19);
-        this.direction = Direction.UP;
     }
 }
 record Position(int x, int y){}
-
-enum Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
-}
